952019b5106f3c78bd3ab2137aa0a4544c42fd7d
Cinthiams <cinbertolo@gmail.com>
2023-03-14 22:27:07 -0300

    atualizando views register_user

--- menbers/views.py
+++ menbers/views.py
@@ -23,7 +23,7 @@ def login_user(request):
     if user is not None:
       login(request,user)
       print(user,'------------DSADSAD----------------')
-      return redirect('Perfil')
+      return redirect('perfil')
           # Redirect to a success page.
     
     else:
@@ -39,17 +39,17 @@ def logout_user(request):
   messages.success(request,'Até logo!')
   return redirect('login')
 #---------------------------------------------------------------------------------------------------------------------------------
+
+  
+
 def register_user(request,**args):
-  new_group = Group.objects.get_or_create(name = 'clientes')
-  #form = createUserForms(request.POST)
+  new_group = Group.objects.get( name="clientes")
   if request.method =='POST':
     username1 = request.POST.get('username')
     email1 = request.POST.get('email')
     password1 = request.POST.get('password1')
     password2 = request.POST.get('password2')
-    print(username1,'--------------------------')
-    #createUserForms()
-    #form = createUserForms(username= username,email=email,password1=password1,password2=password2)
+  
     try:
       
       if password1 != password2:
@@ -61,24 +61,21 @@ def register_user(request,**args):
         print('usuario já existeeeeeeeeeeeeeeeeeeeeeeeee')
         return redirect('registro')
       try:
+        user = User.objects.get_or_create(username=username1, email=email1, password=password1, commit = False)
+        user.groups.add(new_group)
+        user.save()
+  
         
-            user = User.objects.create_user(username=username1, email=email1, password=password1)
-            user.groups.add(new_group)
-            
-            user.save()
-            messages.success("usuario criado com sucesso")
-            print('user criado com sucessooooooooooooooooooo')
-            return redirect(request,'endereco')
+        return redirect('perfil')
       except:
-        pass
+       
+        messages.error(request,"usuario não salvo -------------------------------")
     except:
       messages.error(request,"usuario não definido")
       return render(request,'registro.html') 
 
-      
-    #else:  cinn123@gmail.com cinn3
-    #  form = UserCreationForm()
   return render(request,'registro.html') 
+
 
 def register_address(request):
   usuario = Usuario.objects.get()
@@ -93,6 +90,7 @@ def register_address(request):
        
      
   return render(request,'endereco.html')
+
   
   
 """  if form.is_valid():
@@ -110,44 +108,42 @@ def register_address(request):
   return render(request,'registro.html',{'form':form})"""
 
 
-"""
-  
-class UsuarioCreate(CreateView):
-    template_name = "sessao_3\cad\cadastro.html"
-    form_class = UsuarioForm
-    ##success_url = reverse_lazy('login')
+"""def register_user(request,**args):
+  new_group = Group.objects.get_or_create(name = 'clientes')
+  #form = createUserForms(request.POST)
+  if request.method =='POST':
+    username1 = request.POST.get('username')
+    email1 = request.POST.get('email')
+    password1 = request.POST.get('password1')
+    password2 = request.POST.get('password2')
+    try:
+      
+      if password1 != password2:
+        print('senha incorreta')
+        messages.success(request,'senha diferente da outra')
+        return redirect('registro')
+      if User.objects.filter(email=email1).exists():
+        messages.success(request,'usuario já existe')
+        print('usuario já existeeeeeeeeeeeeeeeeeeeeeeeee')
+        return redirect('registro')
+      try:
+        print('--------------------------------------------------------')
+        user = User.objects.create(username=username1, email=email1, password=password1, commit = False)
+        user.groups.add(new_group)
+          
+        user.save()
+        messages.success("usuario criado com sucesso")
+        print('user criado com sucessooooooooooooooooooo')
+        return redirect(request,'endereco')
+      except:
+        messages.error(request,"usuario não salvo -------------------------------")
+    except:
+      messages.error(request,"usuario não definido")
+      return render(request,'registro.html') 
 
-    def form_valid(self, form):
-  
-        grupo = get_object_or_404(Group, name="cliente")
-        
-        
-        url = super().form_valid(form) 
-        Usuario.objects.create(usuario=self.object)    
-        self.object.groups.add(grupo)
-        self.object.save()
-        
-  
-        return url
+      
+    #else:  cinn123@gmail.com cinn3
+    #  form = UserCreationForm()
+  return render(request,'registro.html') 
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-
-        context['titulo'] = "Registro de novo usuário"
-        context['botao'] = "Cadastrar"
-
-        return context
-
-def UsuarioView(request):
-   
-    usuario = Usuario.objects.get()
-
- 
-    context= {
-                    'foto': usuario.foto,
-                    'name': usuario.name,
-                    'telefone': usuario.telefone,
-                    'rua': usuario.rua,
-                    'cep': usuario.cep }
-    return render(request, 'sessao_3/user/perfil.html',context)
   """
