80f143d900c6f83579dfcd3fa9b78f80fdbb7549
Cinthiams <cinbertolo@gmail.com>
2023-03-16 23:41:25 -0300

    adicionando funcao rastreio

--- correios/urls.py
+++ correios/urls.py
@@ -1,13 +1,13 @@
 from .views import Perfil,Rastreio
 from django.urls import path,include
-
+from . import views
 urlpatterns = [
     #path('cadastro',Usuario.as_view(), name='cadastro'),
    
     
    
     path('perfil', Perfil.as_view(), name='perfil'),
-    path('rastreio', Rastreio.as_view(), name='rastreio'),
+    path('rastreio', views.Rastreio, name='rastreio'),
     
     
     
